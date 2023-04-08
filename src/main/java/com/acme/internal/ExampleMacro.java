100c6041ee5da535a9e0832fb64766a3ecdd4858
Arham Hayat <ar.hayat7@gmail.com>
2023-04-08 17:43:15 +0530

    Fixation -UnderProgress

--- src/main/java/com/acme/internal/ExampleMacro.java
+++ src/main/java/com/acme/internal/ExampleMacro.java
@@ -1,22 +1,3 @@
-/*
- * See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
 package com.acme.internal;
 
 import javax.inject.Named;
@@ -34,6 +15,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+import javax.inject.*;
 
 
 import org.xwiki.bridge.DocumentModelBridge;
@@ -48,12 +30,14 @@ import org.xwiki.rendering.macro.AbstractMacro;
 import org.xwiki.rendering.macro.MacroExecutionException;
 import com.acme.ExampleMacroParameters;
 import org.xwiki.rendering.transformation.MacroTransformationContext;
+import org.xwiki.model.reference.EntityReferenceResolver;
 
 /**
  * Example Macro.
  */
 @Component
 @Named("lda")
+@Singleton
 public class ExampleMacro extends AbstractMacro<ExampleMacroParameters>
 {
     /**
@@ -64,57 +48,100 @@ public class ExampleMacro extends AbstractMacro<ExampleMacroParameters>
     /**
      * Create and initialize the descriptor of the macro.
      */
+
+    @Inject
+    private DocumentModelBridge documentModelBridge;
+
+    @Inject
+    private EntityReferenceResolver<EntityReference> resolver;
+
+
     public ExampleMacro()
     {
         super("This Provides LDA Topic Modelling", DESCRIPTION, ExampleMacroParameters.class);
     }
 
     @Override
-    public List<Block> execute(ExampleMacroParameters docRef, MacroTransformationContext context)
-        throws MacroExecutionException
-    {
-        String result;
+public List<Block> execute(ExampleMacroParameters parameters, String content, MacroTransformationContext context)
+    throws MacroExecutionException
+{
+    EntityReference includedReference = resolve(parameters.getdocRef());
 
-        EntityReference includedReference =
-            resolve(docRef.getdocRef());
-
-        String content;
-        try {
-            content = this.documentModelBridge.getContent(includedReference);
-        } catch (Exception e) {
-            throw new MacroExecutionException(
-                "Failed to load Document",
-                e);
-        }
-        ArrayList<String> contentList = new ArrayList<String>();
-        contentList.add(content);
-        ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
-        pipeList.add(new CharSequenceLowercase());
-        pipeList.add(new CharSequence2TokenSequence());
-        pipeList.add(new TokenSequenceRemoveStopwords());
-        pipeList.add(new TokenSequence2FeatureSequence());
-
-
-        InstanceList instances = new InstanceList(new SerialPipes(pipeList));
-
-
-        for (String content : contentList) {
-            instances.addThruPipe(new Instance(content, null, null, null));
-        }
-
-        // Set up the LDA topic model parameters
-        int numTopics = 10;
-        int numIterations = 1000;
-        int numThreads = 4;
-
-        // Create the LDA topic model
-        ParallelTopicModel model = new ParallelTopicModel(numTopics);
-        model.addInstances(instances);
-        model.setNumThreads(numThreads);
-        model.setNumIterations(numIterations);
-        model.estimate();
-
-
-        return result;
+    String documentContent;
+    try {
+        documentContent = this.documentModelBridge.getContent(includedReference);
+    } catch (Exception e) {
+        throw new MacroExecutionException(
+            "Failed to load Document",
+            e);
     }
+    ArrayList<String> contentList = new ArrayList<String>();
+    contentList.add(documentContent);
+    ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
+    pipeList.add(new CharSequenceLowercase());
+    pipeList.add(new CharSequence2TokenSequence());
+    pipeList.add(new TokenSequenceRemoveStopwords());
+    pipeList.add(new TokenSequence2FeatureSequence());
+
+
+    InstanceList instances = new InstanceList(new SerialPipes(pipeList));
+
+
+    for (String text : contentList) {
+        instances.addThruPipe(new Instance(text, null, null, null));
+    }
+
+    // Set up the LDA topic model parameters
+    int numTopics = 10;
+    int numIterations = 1000;
+    int numThreads = 4;
+
+    // Create the LDA topic model
+    ParallelTopicModel model = new ParallelTopicModel(numTopics);
+    model.addInstances(instances);
+    model.setNumThreads(numThreads);
+    model.setNumIterations(numIterations);
+    model.estimate();
+
+    List<Block> result = new ArrayList<Block>();
+
+    // Create a new paragraph block to contain the results
+    ParagraphBlock paragraph = new ParagraphBlock();
+
+    // Add a word block containing the number of topics
+    WordBlock numTopicsBlock = new WordBlock("Number of topics: " + model.getNumTopics());
+    paragraph.add(numTopicsBlock);
+
+    // Add a word block containing the number of iterations
+    WordBlock numIterationsBlock = new WordBlock("Number of iterations: " + model.getNumIterations());
+    paragraph.add(numIterationsBlock);
+
+    // Add a word block containing the number of threads
+    WordBlock numThreadsBlock = new WordBlock("Number of threads: " + model.getNumThreads());
+    paragraph.add(numThreadsBlock);
+
+    // Add a word block containing the top words for each topic
+    for (int topic = 0; topic < model.getNumTopics(); topic++) {
+        WordBlock topicBlock = new WordBlock("Topic " + topic + ": ");
+        paragraph.add(topicBlock);
+        int[] topicWords = model.getSortedWordsPerTopic(topic);
+        for (int i = 0; i < 5; i++) {
+            if (i > 0) {
+                topicBlock.add(new WordBlock(", "));
+            }
+            topicBlock.add(new WordBlock(instances.getDataAlphabet().lookupObject(topicWords[i]).toString()));
+        }
+    }
+
+    // Add the paragraph block to the result
+    result.add(paragraph);
+
+    return result;
+}
+
+@Override
+public boolean supportsInlineMode() {
+    return false;
+}
+
 }
